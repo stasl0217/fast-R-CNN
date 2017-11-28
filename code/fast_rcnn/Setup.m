95dde57cd9e0291dce15fbe077bec2009abb9394
Shirley Chen <sinternoal@msn.com>
2017-11-27 22:33:34 -0800

    first commit

---
+++ code/fast_rcnn/Setup.m
@@ -0,0 +1,26 @@
+function [] = Setup()
+
+% config = frame_config('','', 'compile');
+config.matconvv_path = '../../';
+cuda_root = ''; 
+% matconvv can automatically guess cuda dir, if not
+% please specify the cuda directory
+
+current_dir = pwd();
+
+cd(fullfile(config.matconvv_path, 'matlab'));
+
+if ispc
+    cuda_method = 'nvcc';
+else
+    cuda_method = 'mex';
+end
+
+if isempty(cuda_root)
+    vl_compilenn('EnableGPU', false, 'CudaMethod', 'nvcc');
+else
+    vl_compilenn('EnableGPU', false, 'CudaRoot', cuda_root, ...
+        'CudaMethod', cuda_method);
+end
+
+cd(current_dir);
